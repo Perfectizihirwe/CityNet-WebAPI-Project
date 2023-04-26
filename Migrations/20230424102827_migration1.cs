c40ea32c76e5d38f2fff93f5c2821967e0edfbbb
Perfectizihirwe <perfectizihirwe@gmail.com>
2023-04-26 09:01:58 +0200

    JWT Authentication

---
+++ Migrations/20230424102827_migration1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CityNet.Migrations
+{
+    public partial class migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+    }
+}
--- Models/CityWithoutPOIDto.cs
+++ Models/CityWithoutPOIDto.cs
@@ -1,9 +1,26 @@
 namespace CityInfo.API.Models
 {
+    // <summary>
+    // A Dto for a city without point of interests
+    // </summary>
     public class CityWithoutPOIDto
     {
+        // <summary>
+        // Id of the city
+        // </summary>
+
         public int Id { get; set; }
+
+        // <summary>
+        // Name of the city
+        // </summary>
+
         public string Name { get; set; } = string.Empty;
+
+        // <summary>
+        // Description of the city
+        // </summary>
+
         public string? Description { get; set; }
     }
 }
